53cef1bfda20d82f49eff5faeefad3f11606326c
keyszl <keyszl@hendrix.edu>
2021-09-11 21:31:49 -0500

    adding first api!

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,1 +1,20 @@
 
+$(document).ready(function() {
+$('#dadjoke').click(function() {
+  $.ajax({
+    dataType: "json",
+    url: "https://random.dog/woof.json",
+    success: function(results) {
+      console.log(results["url"]);
+      if (results["url"].endsWith(".mp4")) {
+        $('#dog').attr("src", "images/blank.png");
+      } else {
+        $('#dog').attr("src", results["url"]);
+      }
+    },
+    error: function(xhr,status,error) {
+      console.log(error);
+    }
+  });
+});
+});
